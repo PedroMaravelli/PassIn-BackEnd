44222af633c3c998f0e4c10599918ae7267ee8ca
PedroMaravelli <phmaravelli123@gmail.com>
2024-04-01 21:27:24 -0300

    'incialization'

---
+++ PassIn.Api/Controllers/EventsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PassIn.Application.UseCases.Events.Register;
+using PassIn.Communication.Requests;
+using PassIn.Communication.Responses;
+
+namespace PassIn.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Register([FromBody] RequestEventJson request) 
+        {
+            try
+            {
+                var useCase = new RegisterEventUseCase();
+
+                useCase.Execute(request);
+
+
+                return Created();
+            }
+            catch (ArgumentException ex) 
+            {
+                return BadRequest(new ResponseErrorJson(ex.Message) );
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new ResponseErrorJson("Unknown error" ));
+            }
+        }
+    }
+}
---
+++ PassIn.Application/UseCases/Events/Register/RegisterEventUseCase.cs
@@ -0,0 +1,26 @@
+﻿using PassIn.Communication.Requests;
+
+namespace PassIn.Application.UseCases.Events.Register
+{
+    public class RegisterEventUseCase
+    {
+        public void Execute(RequestEventJson request)
+        {
+
+
+        }
+
+        private void Validate(RequestEventJson request)
+        {
+            if(request.MaximumAttendees <= 0)
+            {
+                throw new ArgumentException("The maximum attendes is invalid ");
+            }
+
+            if (string.IsNullOrWhiteSpace(request.Title))
+            {
+                throw new ArgumentException("The title is invalid");
+            }
+        }
+    }
+}
